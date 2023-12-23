810eaf93a3f65cbf484d976514e5f98f5782a546
tankboyy <tankboya@naver.com>
2023-12-23 14:26:15 +0900

    달력 일, 토 색상 추가

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,70 +1,54 @@
 'use client';
 
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import dayjs from "dayjs";
+import {makeMonthArr} from "@/utils/makeMonthArr";
 
 const days = ['일', '월', '화', '수', '목', '금', '토'];
 
 export default function Calendar() {
 	const [nowDate, setNowDate] = useState(new Date());
 	const [nowMonth, setNowMonth] = useState(nowDate.getMonth());
+	const [selectDay, setSelectDay] = useState(dayjs(new Date()).format('YYYY-MM-DD'));
 
-	const makeMonthArr = (date: Date) => {
-		const monthArr: number[] = [];
-		const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-		const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+	const [monthArr, setMonthArr] = useState<string[][]>();
 
-		for (let i = 0; i < firstDay.getDay(); i++) {
-			monthArr.push(0);
-		}
-
-		for (let i = 1; i <= lastDay.getDate(); i++) {
-			monthArr.push(i);
-		}
-
-		const chunks: number[][] = [];
-		for (let i = 0; i < monthArr.length; i += 7) {
-			chunks.push(monthArr.slice(i, i + 7));
-		}
-
-		// 첫 주 시작이 일요일이 아닐 경우
-		if (chunks[0].indexOf(0) === 0) {
-			const firstWeek = chunks[0];
-			const prevMonthEndDay = new Date(date.getFullYear(), date.getMonth() - 1, 0).getDate();
-			for (let i = 0; i <= firstWeek.lastIndexOf(0); i++) {
-				firstWeek[i] = prevMonthEndDay + i - firstWeek.lastIndexOf(0);
-			}
-		}
-
-		// 마지막 주 끝이 토요일이 아닐 경우
-		if (chunks[chunks.length - 1].length !== 7) {
-			const lastWeek = chunks[chunks.length - 1];
-			const nextMonthStartDay = new Date(date.getFullYear(), date.getMonth() + 1, 1).getDate();
-			for (let i = lastWeek.length; i < 7; i++) {
-				lastWeek[i] = i;
-			}
-		}
-
-		return chunks;
-	};
+	useEffect(() => {
+		setMonthArr(makeMonthArr(nowDate));
+	}, []);
 
 
 	return (
-		<div>
+		<div className="flex flex-col justify-center">
+			<div className="flex justify-between">
+				<button className="" onClick={() => {
+					setNowDate(new Date(nowDate.getFullYear(), nowDate.getMonth() - 1, nowDate.getDate()));
+				}}> prev
+				</button>
+				<span>
+					{dayjs(nowDate).format('YYYY년 MM월')}
+				</span>
+				<button className="" onClick={() => {
+					setNowDate(new Date(nowDate.getFullYear(), nowDate.getMonth() + 1, nowDate.getDate()));
+				}}> next
+				</button>
+			</div>
 			<div className="flex space-x-6">
 				{days.map((day, index) =>
-					<div key={index} className="w-6 h-6 text-center">
+					<div key={index}
+							 className={`w-6 h-6 text-center ${index === 0 && "text-red-600"} ${index === 6 && "text-blue-500"}`}>
 						{day}
 					</div>
 				)}
 			</div>
 			<div>
 				{makeMonthArr(nowDate).map((week, index) =>
-					<div key={index} className="flex space-x-6" onScroll={(e) => {
-						console.log(e);
-					}}>
+					<div key={index} className="flex space-x-6">
 						{week.map((day, index) =>
-							<div key={index} className="w-6 h-6 text-center">
-								{day}
+							<div key={index}
+									 className={`w-6 h-6 text-center cursor-pointer rounded-full hover:bg-gray-200 ${index === 0 && "text-red-600"} ${index === 6 && "text-blue-500"} ${day === selectDay && "bg-gray-400"}`}
+									 onClick={() => setSelectDay(day)}>
+								{dayjs(day).format('D')}
 							</div>
 						)}
 					</div>
---
+++ src/utils/makeMonthArr.ts
@@ -0,0 +1,46 @@
+import dayjs from "dayjs";
+
+export const makeMonthArr = (date: Date) => {
+	console.log(date);
+	const monthArr: string[] = [];
+	const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+	const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+	const nowYear = date.getFullYear();
+	const nowMonth = date.getMonth();
+
+
+	for (let i = 0; i < firstDay.getDay(); i++) {
+		monthArr.push("");
+	}
+
+	for (let i = 1; i <= lastDay.getDate(); i++) {
+		monthArr.push(dayjs(`${nowYear}-${nowMonth + 1}-${i}`).format('YYYY-MM-DD'));
+	}
+
+	const chunks: string[][] = [];
+	for (let i = 0; i < monthArr.length; i += 7) {
+		chunks.push(monthArr.slice(i, i + 7));
+	}
+
+	console.log(chunks);
+
+	// 첫 주 시작이 일요일이 아닐 경우
+	if (chunks[0].indexOf("") === 0) {
+		const firstWeek = chunks[0];
+		const prevMonthEndDay = new Date(date.getFullYear(), date.getMonth() - 1, 0).getDate();
+		for (let i = 0; i <= firstWeek.lastIndexOf(""); i++) {
+			firstWeek[i] = dayjs(`${nowYear}-${nowMonth}-${prevMonthEndDay - (firstWeek.lastIndexOf("") + 1 - i)}`).format('YYYY-MM-DD');
+		}
+	}
+
+	// 마지막 주 끝이 토요일이 아닐 경우
+	if (chunks[chunks.length - 1].length !== 7) {
+		const lastWeek = chunks[chunks.length - 1];
+		const nextMonthEndDay = new Date(date.getFullYear(), date.getMonth() + 2, 0).getDate();
+		for (let i = lastWeek.length; i < 7; i++) {
+			lastWeek[i] = dayjs(`${nowYear}-${nowMonth + 1}-${nextMonthEndDay + i}`).format('YYYY-MM-DD');
+		}
+	}
+
+	return chunks;
+};
