7a05c7f6ebdfdec50b7c6144b1a9a8d9c407d9f7
tankboyy <tankboya@naver.com>
2024-01-18 19:28:37 +0900

    pwa del

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-
-const runtimeCaching = require('next-pwa/cache');
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
-  disable: process.env.NODE_ENV === 'development',
-});
-
-const nextConfig = withPWA({});
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  }
+};
 
 module.exports = nextConfig;
