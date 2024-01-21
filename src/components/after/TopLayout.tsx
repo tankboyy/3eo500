c317aeba59662ea7b246a9eabbe0106773a48d71
tankboyy <tankboya@naver.com>
2024-01-21 16:57:02 +0900

    햄버거 기능 추가중

--- src/components/after/TopLayout.tsx
+++ src/components/after/TopLayout.tsx
@@ -3,6 +3,15 @@
 import {Button} from "@/components/ui/button";
 import {MoonIcon, SunIcon, HamburgerMenuIcon} from "@radix-ui/react-icons";
 import {useTheme} from "next-themes";
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from "@/components/ui/sheet";
+
 
 export default function TopLayout() {
 	const {theme, resolvedTheme, setTheme} = useTheme();
@@ -14,7 +23,21 @@ export default function TopLayout() {
 	return (
 		<div className="h-[60px] border-b dark:border-border p-[10px] flex justify-between">
 			<Button size="icon" className="bg-transparent">
-				<HamburgerMenuIcon className="h-[1.2rem] w-[1.2rem] stroke-foreground"/>
+				<Sheet>
+					<SheetTrigger>
+						<HamburgerMenuIcon className="h-[1.2rem] w-[1.2rem] stroke-foreground"/>
+					</SheetTrigger>
+					<SheetContent side="left">
+						<SheetHeader>
+							<SheetTitle>Are you absolutely sure?</SheetTitle>
+							<SheetDescription>
+								This action cannot be undone. This will permanently delete your account
+								and remove your data from our servers.
+							</SheetDescription>
+						</SheetHeader>
+					</SheetContent>
+				</Sheet>
+
 			</Button>
 			<Button size="icon">
 				{resolvedTheme !== 'dark' ?
