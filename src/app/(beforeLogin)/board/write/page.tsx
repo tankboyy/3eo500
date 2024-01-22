637e99e68c274f84bcab62fac235ea71f652b5cb
tankboyy <tankboya@naver.com>
2024-01-22 22:40:49 +0900

    s3 연결 성공 cors해결

--- src/app/(beforeLogin)/board/write/page.tsx
+++ src/app/(beforeLogin)/board/write/page.tsx
@@ -4,9 +4,44 @@ import ReactQuill from "react-quill";
 import {useState} from "react";
 import 'react-quill/dist/quill.snow.css';
 import {Button} from "@/components/ui/button";
+import AWS from "aws-sdk";
 
 export default function Page() {
 	const [value, setValue] = useState("");
+
+	const ImageHandler = async () => {
+		const a = document.createElement("input");
+		a.setAttribute('type', 'file');
+		a.setAttribute('accept', 'image/*');
+		a.click();
+		a.addEventListener("change", async () => {
+			const file = a.files?.[0];
+			try {
+				const name = Date.now();
+				const config = {
+					region: process.env.NEXT_PUBLIC_REGION,
+					accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY,
+					secretAccessKey: process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY
+				};
+				console.log(a.files, name, config);
+				AWS.config.update(config);
+
+				const upload = new AWS.S3.ManagedUpload({
+					params: {
+						ACL: 'public-read',
+						Bucket: "3eo500-bucket",
+						Key: `upload/${name}`,
+						Body: file,
+					}
+				});
+				const IMG_URL = await upload.promise().then((res) => res.Location);
+				console.log(IMG_URL);
+			} catch (error) {
+				console.log(error);
+			}
+		});
+	};
+
 	const modules = {
 		toolbar: {
 			container: [
@@ -14,6 +49,9 @@ export default function Page() {
 				[{header: [1, 2, 3, 4, 5, false]}],
 				["bold", "underline"],
 			],
+			handlers: {
+				image: ImageHandler
+			}
 		},
 	};
 	return (
@@ -23,7 +61,8 @@ export default function Page() {
 					작성완료
 				</Button>
 			</div>
-			<ReactQuill theme="snow" value={value} onChange={setValue} className="h-[400px] w-full" modules={modules}/>
+			<ReactQuill theme="snow" value={value} onChange={setValue} className="h-[400px] w-full"
+									modules={modules}/>
 		</main>
 	);
 }
