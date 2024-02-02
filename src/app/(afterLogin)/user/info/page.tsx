a8d4e8373d5cae89a26bcae1b7ec855764396266
tankboyy <tankboya@naver.com>
2024-02-02 19:25:05 +0900

    인풋, 라디오 추가

--- src/app/(afterLogin)/user/info/page.tsx
+++ src/app/(afterLogin)/user/info/page.tsx
@@ -1,7 +1,28 @@
+'use client';
+
+import {Input} from "@/components/ui/input";
+import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group";
+import {Label} from "@/components/ui/label";
+
 export default function Page() {
 	return (
-		<div>
-			hihi
-		</div>
+		<main>
+			<Input type="text" placeholder="닉네임"/>
+			<RadioGroup defaultValue="option-one" className="flex" onValueChange={(value) => console.log(value)}>
+				<div className="flex items-center space-x-2">
+					<RadioGroupItem value="비공개" id="option-one"/>
+					<Label htmlFor="option-one">비공개</Label>
+				</div>
+				<div className="flex items-center space-x-2">
+					<RadioGroupItem value="여자" id="option-two"/>
+					<Label htmlFor="option-two">여자</Label>
+				</div>
+				<div className="flex items-center space-x-2">
+					<RadioGroupItem value="남자" id="option-three"/>
+					<Label htmlFor="option-three">남자</Label>
+				</div>
+			</RadioGroup>
+
+		</main>
 	);
 }
