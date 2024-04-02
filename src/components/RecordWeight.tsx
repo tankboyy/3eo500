b0bbb2d2977448da89ebde351feaa9a63b388bd3
tankboyy <tankboya@naver.com>
2024-04-02 12:55:05 +0900

    컴포넌트화

--- src/components/RecordWeight.tsx
+++ src/components/RecordWeight.tsx
@@ -2,9 +2,6 @@
 
 import React, {useEffect, useState} from "react";
 import {partNames, weightList} from "@/utils/weightList";
-import {AddRecordType, useAddRecord} from "@/hooks/useAddRecord";
-import {useRecoilValue} from "recoil";
-import {selectDateState} from "@/recoil/atoms";
 import {
 	Drawer,
 	DrawerContent,
@@ -14,8 +11,7 @@ import {
 import {Button} from "@/components/ui/button";
 import {ToggleGroup, ToggleGroupItem} from "@/components/ui/toggle-group";
 import {ScrollArea, ScrollBar} from "@/components/ui/scroll-area";
-import {useQueryClient} from "react-query";
-import {Input} from "@/components/ui/input";
+import RecordView from "@/components/record/RecordView";
 
 const _ = require("lodash");
 
@@ -26,35 +22,6 @@ export type recordDataType = { reps: number; weight: number; status: boolean };
 export default function RecordWeight() {
 	const [selectPart, setSelectPart] = useState("");
 	const [recordName, setRecordName] = useState("");
-	const [recordDatas, setRecordDatas] = useState<recordDataType[]>([{
-		reps: 0,
-		weight: 0,
-		status: false
-	}]);
-
-
-	const queryClient = useQueryClient();
-	const selectDate = useRecoilValue(selectDateState);
-
-	useEffect(() => {
-		setRecordDatas([{
-			reps: 0,
-			weight: 0,
-			status: false
-		}]);
-	}, [selectPart, recordName]);
-
-	useEffect(() => {
-		if (recordName) {
-			const data = queryClient.getQueryData<any>('record');
-			if (data[selectDate]) {
-				const recordData = data[selectDate][recordName];
-				if (recordData) {
-					setRecordDatas(recordData);
-				}
-			}
-		}
-	}, [recordName]);
 
 
 	const onChangePart = (name: string) => {
@@ -62,76 +29,15 @@ export default function RecordWeight() {
 		setRecordName("");
 	};
 
-	const onChangeWeight: (e: React.ChangeEvent<HTMLInputElement>, index: number) => void = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
-		setRecordDatas(prev => {
-			const data = [...prev];
-			data[index].weight = Number(e.target.value);
-			return data;
-		});
-	};
-
-	const onChangeReps: (e: React.ChangeEvent<HTMLInputElement>, index: number) => void = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
-		setRecordDatas(prev => {
-			const data = [...prev];
-			data[index].reps = Number(e.target.value);
-			return data;
-		});
-	};
-
-	const onChangeStatus: (index: number) => void = (index: number) => {
-		setRecordDatas(prev => {
-			const data = [...prev];
-			data[index].status = !data[index].status;
-			return data;
-		});
-	};
-
-
-	function removeSelection() {
+	const onClose = () => {
 		setRecordName("");
-	}
-
-	const changeRecords = (action: string) => {
-		if (action === "add") {
-			if (recordDatas.length === 12) return;
-			setRecordDatas(prev => {
-				return [...prev, {
-					reps: 0,
-					weight: 0,
-					status: false
-				}];
-			});
-		} else if (action === "remove") {
-			if (recordDatas.length === 0) return;
-			setRecordDatas(prev => {
-				return prev.slice(0, prev.length - 1);
-			});
-		}
+		setSelectPart("");
 	};
 
-	const {mutation: addRecord} = useAddRecord();
-	const nowDate = useRecoilValue(selectDateState);
 
-
-	function onSaveRecord() {
-		const uid = localStorage.getItem("uid");
-		if (!uid) return;
-		addRecord.mutate({uid, recordName, selectDate: nowDate, data: recordDatas});
-	}
-
-	function onClosed() {
-		const compareArr = [{
-			reps: 0,
-			weight: 0,
-			status: false
-		}];
-		if (recordName && selectPart && !(_.isEqual(recordDatas, compareArr))) onSaveRecord();
-	}
-
-	// ;
 	return (
 		<div id="component">
-			<Drawer onClose={onClosed}>
+			<Drawer onClose={onClose}>
 				<DrawerTrigger>
 					<Button className="text-[11px]">기록 추가하기</Button>
 				</DrawerTrigger>
@@ -169,51 +75,7 @@ export default function RecordWeight() {
 								</div>
 								<ScrollBar orientation="vertical"/>
 							</ScrollArea> :
-							<div className="w-full">
-								{recordName &&
-                  <div className="w-full flex flex-col justify-between p-[20px]">
-                    <div>
-                      <div className="flex justify-between border-b-2 w-full pb-2 mb-6 px-[6px]">
-                        <span>{`${selectPart} | ${recordName}`}</span>
-                        <div>
-                          <button className="hover:bg-red-300 w-[24px] h-[24px] rounded-full"
-                                  onClick={removeSelection}>X
-                          </button>
-                        </div>
-                      </div>
-                      <div className="px-[20px]">
-                        <div className="flex justify-between">
-                          <span>세트</span>
-                          <span>Kg</span>
-                          <span>횟수</span>
-                          <button>완료</button>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="pb-[20px]">{
-											recordDatas?.map((item: recordDataType, index) => (
-												<div className="flex justify-between px-[20px] h-[30px]" key={index}>
-													<button className="w-[28px]">{index + 1}</button>
-													<Input className="w-[40px] px-[4px] text-center" type="text"
-																 onChange={(e) => onChangeWeight(e, index)} defaultValue={item.weight} maxLength={3}/>
-													<Input className="w-[40px] px-[4px] text-center" type="text"
-																 onChange={(e) => onChangeReps(e, index)} defaultValue={item.reps} maxLength={3}/>
-													<button className="w-[28px]"
-																	onClick={() => onChangeStatus(index)}>{item.status ? "O" : "X"}</button>
-												</div>
-											))}
-                    </div>
-                    <div className="flex space-x-2">
-                      <button className="w-1/2 h-8 bg-primary rounded-[8px]"
-                              onClick={() => changeRecords("add")}>세트추가
-                      </button>
-                      <button className="w-1/2 h-8 bg-primary rounded-[8px]"
-                              onClick={() => changeRecords("remove")}>세트삭제
-                      </button>
-                    </div>
-                  </div>
-								}
-							</div>
+							<RecordView recordName={recordName} selectPart={selectPart}/>
 						}
 					</div>
 				</DrawerContent>
