284e1d77f904971c53ffdce378dcbe253baa78de
tankboyy <tankboya@naver.com>
2024-04-02 17:26:52 +0900

    auth가 있으면 user Recoil추가

--- src/hooks/useAuthentication.tsx
+++ src/hooks/useAuthentication.tsx
@@ -1,15 +1,22 @@
-import {useEffect, useState} from "react";
+import {useEffect, useLayoutEffect, useState} from "react";
 import {getAuth, onAuthStateChanged} from "@firebase/auth";
 import {app} from "@/firebase";
+import {useRecoilState} from "recoil";
+import {userDataState} from "@/recoil/atoms";
 
 export default function UseAuthentication() {
-	const [isLoggedIn, setIsLoggedIn] = useState<boolean>();
-	const auth = getAuth();
-	useEffect(() => {
+	const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+	const [userData, setUserData] = useRecoilState(userDataState);
 
+	const auth = getAuth();
+	useLayoutEffect(() => {
 		onAuthStateChanged(auth, (user) => {
-			user ? setIsLoggedIn(true) : setIsLoggedIn(false);
+			if (user) {
+				setIsLoggedIn(true);
+				setUserData(user);
+			} else setIsLoggedIn(false);
 		});
 	}, [auth]);
+
 	return isLoggedIn;
 }
