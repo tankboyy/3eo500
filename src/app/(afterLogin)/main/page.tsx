a446cda59f7e9dae10ecc9cf558b96c39b4de239
tankboyy <tankboya@naver.com>
2024-04-02 12:55:55 +0900

    정리

--- src/app/(afterLogin)/main/page.tsx
+++ src/app/(afterLogin)/main/page.tsx
@@ -1,8 +1,19 @@
+'use client';
+
 import Calendar from "@/components/Calendar";
 import Main from "@/components/Main";
+import {getRecordData, useGetRecord} from "@/hooks/record.hooks";
+import {useRecoilValue} from "recoil";
+import {userDataState} from "@/recoil/atoms";
+import useGetUser from "@/hooks/useGetUser";
+import {getAuth} from "@firebase/auth";
+import {useQueryClient} from "@tanstack/react-query";
 
 
-export default async function Home() {
+export default function Home() {
+
+	useGetRecord();
+
 	return (
 		<main>
 			<Calendar/>
