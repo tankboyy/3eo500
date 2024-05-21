3ac7fce66be557e9797630572362ef80fbf6473c
tankboyy <tankboya@naver.com>
2024-05-21 22:25:55 +0900

    /login css

--- src/components/Sign.tsx
+++ src/components/Sign.tsx
@@ -6,18 +6,20 @@ import Signup from "@/components/signup/signup";
 export default function Sign() {
 
 	return (
-		<Tabs defaultValue="login" className="w-[250px]">
-			<TabsList className="flex space-x-8">
-				<TabsTrigger value="login">로그인</TabsTrigger>
-				<TabsTrigger value="signup">회원가입</TabsTrigger>
-			</TabsList>
-			<TabsContent value="login">
-				<Login/>
-			</TabsContent>
-			<TabsContent value="signup">
-				<Signup/>
-			</TabsContent>
-		</Tabs>
+		<main className="flex justify-center">
+			<Tabs defaultValue="login" className="w-[250px]">
+				<TabsList className="flex space-x-8">
+					<TabsTrigger value="login">로그인</TabsTrigger>
+					<TabsTrigger value="signup">회원가입</TabsTrigger>
+				</TabsList>
+				<TabsContent value="login">
+					<Login/>
+				</TabsContent>
+				<TabsContent value="signup">
+					<Signup/>
+				</TabsContent>
+			</Tabs>
+		</main>
 	);
 }
 
