52edecf7617464af80b143bd70b5b84ad19624bc
tankboyy <tankboya@naver.com>
2024-05-28 20:03:29 +0900

    server action은 form만

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,21 +3,31 @@
 import {adminApp} from "@/admin";
 import {cookies} from "next/headers";
 import {getAuth} from "firebase-admin/auth";
-import Delete from "@/app/delete";
+import {createUserWithEmailAndPassword} from "@firebase/auth";
+import {useAuth} from "@/firebase";
+import {revalidatePath} from "next/cache";
 
-export async function checkAuth() {
+export async function signIn(formData: FormData) {
+}
+
+export async function signUp(queryData: FormData) {
+	const {id, pw} = {
+		id: queryData.get('id') as string,
+		pw: queryData.get('pw') as string
+	};
+	if (id || pw) return;
 	adminApp;
-	const cookieStore = cookies();
-	const accessToken = cookieStore.get("accessToken")?.value;
-	if (!accessToken) {
-		return {isAuthenticated: false};
-	}
-	return getAuth().verifyIdToken(accessToken)
-		.then((decodedToken) => {
-			return {isAuthenticated: true};
+	return createUserWithEmailAndPassword(useAuth, id, pw)
+		.then((userData) => {
+			if (userData) {
+				console.log('success');
+				revalidatePath("/main");
+			}
 		})
 		.catch((error) => {
-			console.log(error);
-			return {isAuthenticated: false};
+			console.log('error');
+			if (error.message === "Firebase: Error (auth/email-already-in-use).") {
+				return;
+			}
 		});
 }
