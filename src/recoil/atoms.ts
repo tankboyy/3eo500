60596fad9b670b1309d79d8e3b8ca862a9fae4ea
tankboyy <tankboya@naver.com>
2024-05-31 15:42:13 +0900

    recoil - selector 추가

--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -1,4 +1,4 @@
-import {atom} from "recoil";
+import {atom, selector} from "recoil";
 import dayjs from "dayjs";
 import {apiBoardType} from "@/utils/types";
 import {User} from "@firebase/auth";
@@ -19,9 +19,40 @@ export const recordDataState = atom<recordType>({
 });
 
 
-export const selectDateState = atom<string>({
+export const selectDateState = atom<string | Date>({
 	key: 'selectDateState',
-	default: dayjs().format("YYYY-MM-DD")
+	default: dayjs().format("YYYY-MM-DD"),
+});
+
+function isValidDateFormat(dateString: string) {
+	const regex = /^\d{4}-\d{2}-\d{2}$/;
+	if (!regex.test(dateString)) {
+		return false;
+	}
+
+	// 날짜 유효성 검사를 추가로 수행합니다.
+	const date = new Date(dateString);
+	const year = dateString.substring(0, 4);
+	const month = dateString.substring(5, 7);
+	const day = dateString.substring(8, 10);
+
+	return date.getFullYear() === parseInt(year) &&
+		(date.getMonth() + 1) === parseInt(month) &&
+		date.getDate() === parseInt(day);
+}
+
+export const selectorDateState = selector({
+	key: 'selectorDate',
+	get: ({get}) => {
+		return get(selectDateState);
+	},
+	set: ({set}, newValue) => {
+		if (typeof newValue === "string" && isValidDateFormat(newValue)) {
+			set(selectDateState, newValue);
+		} else if (newValue instanceof Date) {
+			set(selectDateState, dayjs(newValue).format("YYYY-MM-DD"));
+		}
+	}
 });
 
 export type userType = {
