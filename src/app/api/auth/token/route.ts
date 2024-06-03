b4d46c271e05ddd59876b6b6d3fc7e805c0e9fd6
tankboyy <tankboya@naver.com>
2024-06-03 20:40:16 +0900

    accessToken을 받아 유효한지 확인하는 apiˆ

--- src/app/api/auth/token/route.ts
+++ src/app/api/auth/token/route.ts
@@ -1,9 +1,7 @@
 import useGetUid from "@/hooks/server/useGetUid";
 
 export async function POST(request: Request) {
-	console.log('POST');
 	const {accessToken} = await request.json().then((data: { accessToken: string }) => data);
-	
-	await useGetUid("asd");
-	return Response.json({accessToken});
+	const data = await useGetUid(accessToken);
+	return Response.json({data: data});
 }
