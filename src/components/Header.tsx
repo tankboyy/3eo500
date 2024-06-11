ddba8791a55e198b2877ca055e977bf846dcd954
tankboyy <tankboya@naver.com>
2024-06-11 15:34:14 +0900

    accessToken 유무로 로그인 버튼 표시

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,11 @@ import MobileNav from "@/components/MobileNav";
 import Nav from "@/components/Nav";
 import Link from "next/link";
 import ThemeChanger from "@/components/ThemeChanger";
-import {useRecoilState, useRecoilValue} from "recoil";
-import {userDataState} from "@/recoil/atoms";
-import {useAuth} from "@/firebase";
-import {useEffect, useLayoutEffect, useState} from "react";
+import {cookies} from "next/headers";
 
 
 export default function Header() {
-
+	const accessToken = cookies().get('accessToken')?.value;
 	return (
 		<header
 			className="h-14 px-5 sticky top-0 z-50 w-full border-b flex justify-between items-center bg-background dark:border-border">
@@ -18,7 +15,7 @@ export default function Header() {
 				<Nav/>
 			</div>
 			<div className="flex items-center gap-1">
-				<Link href="/login" className={`${'' && "hidden"}  text-xs font-medium`}>
+				<Link href="/login" className={`${accessToken && "hidden"}  text-xs font-medium`}>
 					로그인
 				</Link>
 				<ThemeChanger/>
