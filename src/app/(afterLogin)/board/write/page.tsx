ab1665787fe0c3349bf3f1e93b12a3cb1f062414
오석중 <dol@oseogjungs-MacBook-Pro.local>
2024-06-25 18:06:22 +0900

    게시글 동영상 저장 추가

--- src/app/(afterLogin)/board/write/page.tsx
+++ src/app/(afterLogin)/board/write/page.tsx
@@ -6,6 +6,10 @@ import 'react-quill/dist/quill.snow.css';
 import {Button} from "@/components/ui/button";
 import AWS from "aws-sdk";
 import {Input} from "@/components/ui/input";
+import PageLayout from "@/components/PageLayout";
+import {Delta} from "quill";
+import dynamic from "next/dynamic";
+
 
 export default function Page() {
 	const [data, setData] = useState("");
@@ -18,7 +22,66 @@ export default function Page() {
 
 	useEffect(() => {
 		setIsOpen(true);
-	}, [])
+	}, []);
+
+	const VideoHandler = async () => {
+		const a = document.createElement("input");
+		a.setAttribute('type', 'file');
+		a.setAttribute('accept', "video/mp4,video/mkv, video/x-m4v,video/*");
+		a.click();
+		a.addEventListener("change", async () => {
+			const file = a.files?.[0];
+			const url = URL.createObjectURL(file!);
+			const editor = quillRef.current?.getEditor();
+			const range = editor.getSelection();
+
+			const video = document.createElement('video');
+			video.src = url;
+
+			const iframe = document.createElement('iframe');
+			iframe.className = 'ql-video';
+			iframe.setAttribute('frameborder', '0');
+			iframe.setAttribute('allowfullscreen', 'true');
+			iframe.src = url;
+
+			console.log(iframe.outerHTML);
+
+			setData(prev => prev + `<iframe src=${url} frameborder='0' allowfullscreen='true'></iframe>`);
+			// setData((prev) => prev + video.outerHTML);
+			//
+			// const asd = process.env.NEXT_PUBLIC_CF_URL + 'https://3eo500-bucket.s3.ap-northeast-2.amazonaws.com/video/1719304952714';
+			// videoRef.current!.src = asd;
+			//
+			try {
+				if (!file) return;
+				const name = Date.now();
+				const config = {
+					region: process.env.NEXT_PUBLIC_REGION,
+					accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY,
+					secretAccessKey: process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY
+				};
+				AWS.config.update(config);
+				const upload = new AWS.S3.ManagedUpload({
+					params: {
+						ACL: 'public-read',
+						Bucket: "3eo500-bucket",
+						Key: `video/${name}`,
+						Body: file,
+						ContentType: file.type
+					}
+				});
+				const url_key = await upload.promise().then((res) => res.Key);
+				// @ts-ignore
+				const editor = quillRef.current?.getEditor();
+				const range = editor.getSelection();
+				videoRef.current!.src = url_key;
+				// editor.insertEmbed(range.index, 'video', process.env.NEXT_PUBLIC_CF_URL + url_key);
+			} catch (error) {
+				console.log(error);
+			}
+		});
+
+	};
 
 	const ImageHandler = async () => {
 		const a = document.createElement("input");
@@ -27,6 +90,8 @@ export default function Page() {
 		a.click();
 		a.addEventListener("change", async () => {
 			const file = a.files?.[0];
+			console.log(file);
+
 			try {
 				const name = Date.now();
 				const config = {
@@ -59,11 +124,13 @@ export default function Page() {
 			toolbar: {
 				container: [
 					["image"],
+					["video"],
 					[{header: [1, 2, 3, 4, 5, false]}],
 					["bold", "underline"],
 				],
 				handlers: {
-					image: ImageHandler
+					image: ImageHandler,
+					video: VideoHandler
 				}
 			},
 		};
@@ -92,22 +159,48 @@ export default function Page() {
 		setTitle(e.target.value);
 	};
 
+	let videoRef = useRef(null);
 	return (
-		<main className="py-[20px]">
+		<PageLayout className="gap-0">
 			{
-				!!ReactQuill && isOpen && 
-				<>
-				<div className="flex w-full space-x-2 pb-[10px]">
-				<Input placeholder="제목" onChange={onChangeTitle}/>
-				<Button onClick={handleSubmit}>
-					작성완료
-				</Button>
-			</div>
-			<ReactQuill ref={quillRef} theme="snow" value={data} onChange={setData} className="h-[400px] w-full"
-									modules={modules}/>
-				</>
+				!!ReactQuill && isOpen &&
+        <>
+          <div className="flex w-full space-x-2 pb-[10px]">
+            <button onClick={() => {
+
+							console.log(data);
+							const asd = process.env.NEXT_PUBLIC_CF_URL + 'video/1719305333030';
+							// setData(prev => prev + `<iframe src=${asd} frameborder='0' allowfullscreen='true'></iframe>`);
+							// videoRef.current!.src = asd;
+							// console.log(quillRef.current?.getEditor());
+							// const delta = [
+							// 	{
+							// 		insert: {
+							// 			video: "blob:http://localhost:3000/451df8f1-313b-43d0-8056-6b5a752e150b"
+							// 		},
+							// 		attributes: {
+							// 			width: '200',
+							// 			height: '200'
+							// 		}
+							// 	},
+							// ];
+							quillRef.current?.getEditor().insertEmbed(0, 'video', asd);
+						}}>asd
+            </button>
+            <Input placeholder="제목" onChange={onChangeTitle}/>
+            <Button onClick={handleSubmit}>
+              작성완료
+            </Button>
+          </div>
+          <ReactQuill ref={quillRef} theme="snow" value={data} onChange={setData} className="h-[400px] w-full"
+                      modules={modules}/>
+        </>
 			}
-		</main>
+			<video ref={videoRef}>
+
+			</video>
+
+		</PageLayout>
 	);
 }
 
