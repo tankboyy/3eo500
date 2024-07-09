fdf9f603e880cb44276c6e0c5ed205f873def47c
오석중 <dol@oseogjungs-MacBook-Pro.local>
2024-07-09 15:19:40 +0900

    footer css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type {Metadata} from 'next';
 import './globals.css';
 import Provider from "@/components/Provider";
 import Header from "@/components/Header";
-import {auth} from "@/auth";
 
 
 export const metadata: Metadata = {
@@ -15,19 +14,21 @@ export const metadata: Metadata = {
 export default async function RootLayout({children}: {
 	children: React.ReactNode
 }) {
-	console.log("zzz", await auth(), 'zzz');
+
 	return (
 		<html lang="ko">
-		<body className="">
+		<body className="h-screen">
 		<Provider>
 			<main className="flex flex-col mx-auto max-w-screen-md min-w-[320px]">
 				<Header/>
 				{children}
 			</main>
+			<div className="relative py-16">
+				<footer className="w-full flex justify-center text-sm text-gray-500">
+					운동을 기록해보자
+				</footer>
+			</div>
 		</Provider>
-		<footer className="w-full flex justify-center text-sm text-gray-500 ">
-			운동을 기록해보자
-		</footer>
 		</body>
 		</html>
 	);
